17341e921334a6ea232b414a88786a7251cdec33
whitered010 <142947051+whitered010@users.noreply.github.com>
2024-11-07 17:30:40 +0200

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        'spotify-green': '#1DB954',
+      },
+    },
+  },
+  plugins: [],
+};
